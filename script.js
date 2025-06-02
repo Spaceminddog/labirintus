37c94d1a0c822a5b97fbb152346c51707614ca65
nocodenorbert <115705191+Spaceminddog@users.noreply.github.com>
2025-06-02 17:17:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function handlePrimaryClick() {
+    alert("Köszönjük, hogy érdeklődsz! Hamarosan felvesszük veled a kapcsolatot.");
+  }
+  
+  function handleSecondaryClick() {
+    alert("Kérlek, töltsd ki az űrlapot a konzultáció időpont egyeztetéséhez.");
+  }
+  
