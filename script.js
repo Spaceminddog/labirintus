2edadaa0ba0102d59dd0838a2dea31107d6f322c
nocodenorbert <115705191+Spaceminddog@users.noreply.github.com>
2025-06-02 17:29:27 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function handlePrimaryClick() {
-    alert("Köszönjük, hogy érdeklődsz! Hamarosan felvesszük veled a kapcsolatot.");
-  }
-  
-  function handleSecondaryClick() {
-    alert("Kérlek, töltsd ki az űrlapot a konzultáció időpont egyeztetéséhez.");
-  }
-  
